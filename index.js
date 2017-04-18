287e72452e9aeab25f40e00bde6f5e6f933ed02e
naqvitalha <naqvitalha@gmail.com>
2017-04-18 14:30:20 +0530

    MOBILEWEB-161 | created npm config

---
+++ index.js
@@ -0,0 +1,8 @@
+import RecyclerListView from "./recyclerlistview/RecyclerListView";
+import DataProvider from "./recyclerlistview/dependencies/DataProvider";
+import LayoutProvider from "./recyclerlistview/dependencies/LayoutProvider";
+export default{
+    RecyclerListView,
+    DataProvider,
+    LayoutProvider
+}
