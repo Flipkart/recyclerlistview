63b884cf0cd5210667f8853ea64dd17edc97358e
siddhantsoni <soni.siddhant@flipkart.com>
2017-11-03 03:01:47 +0530

    Documented the externalScrollViewer prop in propTypes

--- src/core/RecyclerListView.js
+++ src/core/RecyclerListView.js
@@ -467,6 +467,10 @@ RecyclerListView.propTypes = {
     //On scroll callback onScroll(rawEvent, offsetX, offsetY), note you get offsets no need to read scrollTop/scrollLeft
     onScroll: PropTypes.func,
 
+    //Provide your own ScrollView Component. The contract for the scroll event should match the native scroll event contract, i.e.
+    // event = { nativeEvent: { contentOffset: { x: offset, y: offset } } }
+    externalScrollViewer: PropTypes.func,
+
     //Callback given when user scrolls to the end of the list or footer just becomes visible, useful in incremental loading scenarios
     onEndReached: PropTypes.func,
 
