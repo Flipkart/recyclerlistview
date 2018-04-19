f23b36d936cb706ab905131a3dacc401e9b9e883
naqvitalha <naqvitalha@gmail.com>
2018-04-19 14:15:30 +0530

    Creating a way to pass props down to inner scrollview with type safety

--- src/core/RecyclerListView.tsx
+++ src/core/RecyclerListView.tsx
@@ -97,6 +97,10 @@ export interface RecyclerListViewProps {
     forceNonDeterministicRendering?: boolean;
     extendedState?: object;
     itemAnimator?: ItemAnimator;
+
+    //For all props that need to be proxied to inner/external scrollview. Put them in an object and they'll be spread
+    //and passed down. For better typescript support.
+    scrollViewProps?: object;
 }
 export interface RecyclerListViewState {
     renderStack: RenderStack;
@@ -269,6 +273,7 @@ export default class RecyclerListView extends React.Component<RecyclerListViewPr
             <ScrollComponent
                 ref={(scrollComponent) => this._scrollComponent = scrollComponent as BaseScrollComponent | null}
                 {...this.props}
+                {...this.props.scrollViewProps}
                 onScroll={this._onScroll}
                 onSizeChanged={this._onSizeChanged}
                 contentHeight={this._initComplete ? this._virtualRenderer.getLayoutDimension().height : 0}
