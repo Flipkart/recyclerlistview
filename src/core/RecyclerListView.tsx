672c27f7fd900cd6ede0840b76a9b05539ca957e
naqvitalha <naqvitalha@gmail.com>
2018-05-22 10:00:13 +0530

    Removed lodash-es

--- src/core/RecyclerListView.tsx
+++ src/core/RecyclerListView.tsx
@@ -18,7 +18,7 @@
  * TODO: Observe size changes on web to optimize for reflowability
  * TODO: Solve //TSI
  */
-import debounce from "lodash-es/debounce";
+import debounce = require("lodash.debounce");
 import * as PropTypes from "prop-types";
 import * as React from "react";
 import { ObjectUtil, Default } from "ts-object-utils";
