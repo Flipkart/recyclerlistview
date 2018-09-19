7138d1b34095976a197834aa5f523157ffd04082
Ananya Chandra <ananya.chandra@Ananya-Chandra.local>
2018-09-19 14:49:18 +0530

    Using view from row renderer.

--- src/core/StickyContainer.tsx
+++ src/core/StickyContainer.tsx
@@ -8,17 +8,20 @@ import RecyclerListView, {RecyclerListViewState, RecyclerListViewProps} from "./
 
 export interface StickyContainerProps {
     children: RecyclerListView<RecyclerListViewProps, RecyclerListViewState>;
-    stickyIndices?: number;
+    stickyIndices: number;
     stickyView?: JSX.Element;
 }
 export interface StickyContainerState {
     visible: boolean;
 }
 export default class StickyContainer<P extends StickyContainerProps, S extends StickyContainerState> extends React.Component<P, S> {
+    private rowRenderer: (type: string | number, data: any, index: number) => JSX.Element | JSX.Element[] | null;
 
     constructor(props: P, context?: any) {
         super(props, context);
         this.onVisibleIndexesChanged = this.onVisibleIndexesChanged.bind(this);
+        const recycler: RecyclerListView<RecyclerListViewProps, RecyclerListViewState> = this.props.children;
+        this.rowRenderer = recycler.props.rowRenderer;
 
         this.state = {
             visible: false,
@@ -33,7 +36,11 @@ export default class StickyContainer<P extends StickyContainerProps, S extends S
                     {...this.props.children.props}
                     onVisibleIndexesChanged={this.onVisibleIndexesChanged}
                 />
-                {value ? <View style={{height: 200, width: 300, backgroundColor: "blue", position: "absolute", top: 0}}/> : null}
+                {value ?
+                    <View style={{position: "absolute", top: 0}}>
+                        {this.rowRenderer(0, this.props.stickyIndices, 0)}
+                    </View> : null}
+                {/*{value ? <View style={{height: 200, width: 300, backgroundColor: "blue", position: "absolute", top: 0}}/> : null}*/}
             </View>
         );
     }
@@ -51,10 +58,12 @@ export default class StickyContainer<P extends StickyContainerProps, S extends S
     }
 
     private onVisibleIndexesChanged(all: number[], now: number[], notNow: number[]): void {
-        if (notNow[0] === 1) {
-            this.show();
-        } else if (now[0] === 1) {
-            this.hide();
+        if (this.props.stickyIndices) {
+            if (notNow[0] === this.props.stickyIndices - 1) {
+                this.show();
+            } else if (now[0] === this.props.stickyIndices - 1) {
+                this.hide();
+            }
         }
     }
 }
