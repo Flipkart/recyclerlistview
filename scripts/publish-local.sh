0132c4d1c2a01a27971694cc0060106b86965dc3
Ananya Chandra <ananya.chandra@Ananya-Chandra.local>
2019-02-06 19:09:34 +0530

    Versions.

--- scripts/publish-local.sh
+++ scripts/publish-local.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 set -e
 
-TARGET=$"/Users/ananya.chandra/Documents/ReactNativeRepos/ts-rex/node_modules/recyclerlistview/dist" #target-path
+TARGET=$"/Users/talha.naqvi/Documents/Work/RLV-Demo/node_modules/recyclerlistview/dist" #target-path
+#TARGET=$"/Users/ananya.chandra/Documents/ReactNativeRepos/ts-rex/node_modules/recyclerlistview/dist" #target-path
 #TARGET=$"/Users/ananya.chandra/Documents/ReactNativeRepos/ugc-react-native/node_modules/recyclerlistview/dist" #target-path
 
 npm run build
