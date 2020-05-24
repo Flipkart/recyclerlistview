4a83eea8e9ff7c24c18cdb90ed06d88cf3cf98b6
alstjr7375 <alstjr7375@daum.net>
2020-05-24 12:00:58 +0900

    Fix List<JSX.Element> type

--- src/core/RecyclerListView.tsx
+++ src/core/RecyclerListView.tsx
@@ -39,11 +39,11 @@ import ItemAnimator, { BaseItemAnimator } from "./ItemAnimator";
 import { DebugHandlers } from "..";
 import { ComponentCompat } from "../utils/ComponentCompat";
 //#if [REACT-NATIVE]
-//import ScrollComponent from "../platform/reactnative/scrollcomponent/ScrollComponent";
-//import ViewRenderer from "../platform/reactnative/viewrenderer/ViewRenderer";
-//import { DefaultJSItemAnimator as DefaultItemAnimator } from "../platform/reactnative/itemanimators/defaultjsanimator/DefaultJSItemAnimator";
-//import { Platform } from "react-native";
-//const IS_WEB = !Platform || Platform.OS === "web";
+import ScrollComponent from "../platform/reactnative/scrollcomponent/ScrollComponent";
+import ViewRenderer from "../platform/reactnative/viewrenderer/ViewRenderer";
+import { DefaultJSItemAnimator as DefaultItemAnimator } from "../platform/reactnative/itemanimators/defaultjsanimator/DefaultJSItemAnimator";
+import { Platform } from "react-native";
+const IS_WEB = !Platform || Platform.OS === "web";
 //#endif
 
 /***
@@ -51,10 +51,10 @@ import { ComponentCompat } from "../utils/ComponentCompat";
  */
 
 //#if [WEB]
-import ScrollComponent from "../platform/web/scrollcomponent/ScrollComponent";
-import ViewRenderer from "../platform/web/viewrenderer/ViewRenderer";
-import { DefaultWebItemAnimator as DefaultItemAnimator } from "../platform/web/itemanimators/DefaultWebItemAnimator";
-const IS_WEB = true;
+//import ScrollComponent from "../platform/web/scrollcomponent/ScrollComponent";
+//import ViewRenderer from "../platform/web/viewrenderer/ViewRenderer";
+//import { DefaultWebItemAnimator as DefaultItemAnimator } from "../platform/web/itemanimators/DefaultWebItemAnimator";
+//const IS_WEB = true;
 //#endif
 
 /***
