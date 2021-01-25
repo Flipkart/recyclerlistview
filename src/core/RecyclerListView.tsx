782e6ebb0ed944a653e8c83eac9329cfa243410c
anirudhagarwal365 <anirudh.wish@gmail.com>
2021-01-25 17:06:46 +0530

    Added bring to focus functionality (#585)
    * Added bring to focus functionality
    
    * Added return type for bring to focus
    
    * Added doc for bringToFocus
    
    * Fixed tslint issue
    
    Co-authored-by: Anirudh Agarwal <anirudh.agarwal@flipkart.com>
    Co-authored-by: Talha Naqvi <naqvitalha@gmail.com>

--- src/core/RecyclerListView.tsx
+++ src/core/RecyclerListView.tsx
@@ -242,6 +242,33 @@ export default class RecyclerListView<P extends RecyclerListViewProps, S extends
         }
     }
 
+    /**
+     * This API is almost similar to scrollToIndex, but differs when the view is already in viewport.
+     * Instead of bringing the view to the top of the viewport, it will calculate the overflow of the @param index
+     * and scroll to just bring the entire view to viewport.
+     */
+    public bringToFocus(index: number, animate?: boolean): void {
+        const listSize = this.getRenderedSize();
+        const itemLayout = this.getLayout(index);
+        const currentScrollOffset = this.getCurrentScrollOffset();
+        const {isHorizontal} = this.props;
+        if (itemLayout) {
+            const mainAxisLayoutDimen = isHorizontal ? itemLayout.width : itemLayout.height;
+            const mainAxisLayoutPos = isHorizontal ? itemLayout.x : itemLayout.y;
+            const mainAxisListDimen = isHorizontal ? listSize.width : listSize.height;
+            const screenEndPos = mainAxisListDimen + currentScrollOffset;
+            if (mainAxisLayoutDimen > mainAxisListDimen || mainAxisLayoutPos < currentScrollOffset || mainAxisLayoutPos > screenEndPos) {
+                this.scrollToIndex(index);
+            } else {
+                const viewEndPos = mainAxisLayoutPos + mainAxisLayoutDimen;
+                if (viewEndPos > screenEndPos) {
+                    const offset = viewEndPos - screenEndPos;
+                    this.scrollToOffset(0, offset + currentScrollOffset, animate);
+                }
+            }
+        }
+    }
+
     public scrollToItem(data: any, animate?: boolean): void {
         const count = this.props.dataProvider.getSize();
         for (let i = 0; i < count; i++) {
