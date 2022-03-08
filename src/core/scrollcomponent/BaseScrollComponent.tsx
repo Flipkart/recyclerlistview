2ed1bee79dbd4fd687b0b18c9a2ba8f10a382d62
Talha Naqvi <naqvitalha@gmail.com>
2022-03-08 12:53:30 -0800

    Expose scrollable nodes (#690)
    * PLV now render after first onLayout
    
    * ..
    
    * scrollable node now a public API
    
    Co-authored-by: Talha Naqvi <talha.naqvi@shopify.com>

--- src/core/scrollcomponent/BaseScrollComponent.tsx
+++ src/core/scrollcomponent/BaseScrollComponent.tsx
@@ -20,4 +20,9 @@ export interface ScrollComponentProps {
 }
 export default abstract class BaseScrollComponent extends React.Component<ScrollComponentProps, {}> {
     public abstract scrollTo(x: number, y: number, animate: boolean): void;
+
+    //Override and return node handle to your custom scrollview. Useful if you need to use Animated Events.
+    public getScrollableNode(): number | null {
+        return null;
+    }
 }
