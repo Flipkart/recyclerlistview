bd9e3f6d85e3dce23e3c6e1cf4739a63b8b4c27e
Kanishk Gupta <kanishkgupta1234@gmail.com>
2022-05-17 14:14:56 +0530

    fixed rlv for web

--- src/platform/web/viewrenderer/ViewRenderer.tsx
+++ src/platform/web/viewrenderer/ViewRenderer.tsx
@@ -1,7 +1,13 @@
 import * as React from "react";
 import { CSSProperties } from "react";
+import { LayoutChangeEvent, View, ViewProperties, ViewStyle } from "react-native";
 import { Dimension } from "../../../core/dependencies/LayoutProvider";
 import BaseViewRenderer, { ViewRendererProps } from "../../../core/viewrenderer/BaseViewRenderer";
+
+interface RLVHTMLDivElement extends HTMLDivElement {
+    clientHeight: number;
+    clientWidth: number;
+}
 
 /***
  * View renderer is responsible for creating a container of size provided by LayoutProvider and render content inside it.
@@ -11,7 +17,8 @@ import BaseViewRenderer, { ViewRendererProps } from "../../../core/viewrenderer/
  */
 export default class ViewRenderer extends BaseViewRenderer<any> {
     private _dim: Dimension = { width: 0, height: 0 };
-    private _mainDiv: HTMLDivElement | null = null;
+    private _viewRef: React.Component<ViewProperties, React.ComponentState> | RLVHTMLDivElement | null = null;
+
     public componentDidMount(): void {
         if (super.componentDidMount) {
             super.componentDidMount();
@@ -43,17 +50,17 @@ export default class ViewRenderer extends BaseViewRenderer<any> {
                 ...this.animatorStyleOverrides,
             };
         return (
-            <div ref={this._setRef} style={style}>
+            <View ref={this._setRef} style={style as ViewStyle} onLayout={this._onLayout}>
                 {this.renderChild()}
-            </div>
+            </View>
         );
     }
 
     protected getRef(): object | null {
-        return this._mainDiv;
+        return this._viewRef;
     }
-    private _setRef = (div: HTMLDivElement | null): void => {
-        this._mainDiv = div;
+    private _setRef = (view: React.Component<ViewProperties, React.ComponentState> | RLVHTMLDivElement | null): void => {
+        this._viewRef = view;
     }
     private _getTransform(): string {
         return "translate(" + this.props.x + "px," + this.props.y + "px)";
@@ -61,10 +68,14 @@ export default class ViewRenderer extends BaseViewRenderer<any> {
 
     private _checkSizeChange(): void {
         if (this.props.forceNonDeterministicRendering && this.props.onSizeChanged) {
-            const mainDiv = this._mainDiv;
+            const mainDiv = this._viewRef;
             if (mainDiv) {
-                this._dim.width = mainDiv.clientWidth;
-                this._dim.height = mainDiv.clientHeight;
+                if ("clientWidth" in mainDiv) {
+                    this._dim.width = mainDiv.clientWidth;
+                }
+                if ("clientHeight" in mainDiv) {
+                    this._dim.height = mainDiv.clientHeight;
+                }
                 if (this.props.width !== this._dim.width || this.props.height !== this._dim.height) {
                     this.props.onSizeChanged(this._dim, this.props.index);
                 }
@@ -78,6 +89,25 @@ export default class ViewRenderer extends BaseViewRenderer<any> {
             this.props.onItemLayout(this.props.index);
         }
     }
+
+    private _onLayout = (event: LayoutChangeEvent): void => {
+        //Preventing layout thrashing in super fast scrolls where RN messes up onLayout event
+        const xDiff = Math.abs(this.props.x - event.nativeEvent.layout.x);
+        const yDiff = Math.abs(this.props.y - event.nativeEvent.layout.y);
+        if (xDiff < 1 && yDiff < 1 &&
+            (this.props.height !== event.nativeEvent.layout.height ||
+                this.props.width !== event.nativeEvent.layout.width)) {
+            this._dim.height = event.nativeEvent.layout.height;
+            this._dim.width = event.nativeEvent.layout.width;
+            if (this.props.onSizeChanged) {
+                this.props.onSizeChanged(this._dim, this.props.index);
+            }
+        }
+
+        if (this.props.onItemLayout) {
+            this.props.onItemLayout(this.props.index);
+        }
+    }
 }
 
 const styles: { [key: string]: CSSProperties } = {
@@ -87,11 +117,9 @@ const styles: { [key: string]: CSSProperties } = {
         borderStyle: "solid",
         boxSizing: "border-box",
         display: "flex",
-        flexDirection: "column",
         margin: 0,
         padding: 0,
         position: "absolute",
-        minHeight: 0,
         minWidth: 0,
         left: 0,
         top: 0,
