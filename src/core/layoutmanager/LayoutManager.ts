49450bf29d7fda211188fef01a82e2dad080b871
Talha Naqvi <talha.naqvi@shopify.com>
2022-10-12 13:31:42 -0700

    Increase tolerance inside layout manager to avoid issues with decimal estimates

--- src/core/layoutmanager/LayoutManager.ts
+++ src/core/layoutmanager/LayoutManager.ts
@@ -214,7 +214,7 @@ export class WrapGridLayoutManager extends LayoutManager {
     }
 
     private _checkBounds(itemX: number, itemY: number, itemDim: Dimension, isHorizontal: boolean): boolean {
-        return isHorizontal ? (itemY + itemDim.height <= this._window.height) : (itemX + itemDim.width <= this._window.width);
+        return isHorizontal ? (itemY + itemDim.height <= this._window.height + 1) : (itemX + itemDim.width <= this._window.width + 1);
     }
 }
 
